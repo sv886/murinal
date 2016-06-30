71a1cf5e3f52d99e034156a4ac3b93ba4525879e
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-06-30 11:12:37 -0500

    install simple_form, config form for new post, update post index html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,22 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new post_params
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +26,9 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def post_params
+    params.require(:post).permit(:photo, :description)
   end
 end
