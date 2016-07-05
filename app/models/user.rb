658c69dbe09adfb1d65cac33f306c4a6e5ab5571
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2016-07-05 08:51:15 -0500

    remove user ctrlr/views for devise ctrlr/views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, :username, presence: true
+  validates :email, presence: true
 
   has_many :posts
 end
